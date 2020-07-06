82290e93b977d73eb34c15eecacef396f303adc3
benschulman <bgschulman31@gmail.com>
2020-07-06 16:22:15 -0500

    refactored and added Hand class

--- src/game.py
+++ src/game.py
@@ -11,7 +11,8 @@ SORTED_DECK = [
 # Rank order Constant
 RANK = '23456789TJQKA'
 
-class Hand(enum.Enum):
+# Score of each type of Hand as an Enum
+class Score(enum.Enum):
     HIGHCARD = 1
     PAIR = 2
     TWOPAIR = 3
@@ -21,6 +22,59 @@ class Hand(enum.Enum):
     FULLHOUSE = 7
     FOUROFAKIND = 8
     STRAIGHTFLUSH = 9
+
+class Hand:
+    def __init__(self, hand):
+        self.score = self._calc_score(hand)
+        self.rank = self._calc_rank(hand)
+    
+    @staticmethod
+    def _isStraightFlush(hand):
+        pass
+
+    @staticmethod
+    def _isFourOfAKind(hand):
+        pass
+
+    @staticmethod
+    def _isFullHouse(hand):
+        pass
+
+    @staticmethod
+    def _isFlush(hand):
+        pass
+
+    @staticmethod
+    def _isStraight(hand):
+        pass
+    
+    @staticmethod
+    def _isThreeOfAKind(hand):
+        pass
+
+    @staticmethod
+    def _isTwoPair(hand):
+        pass
+
+    @staticmethod
+    def _isPair(hand):
+        pass
+   
+    def _calc_score(self, hand) -> Score:
+        if not len(hand) == 5:
+            raise Exception("Incorrect hand length")
+        # TODO: Finish _calc_score method
+        return Score.HIGHCARD
+    
+    def _calc_rank(self, hand) -> tuple:
+        if not len(hand) == 5:
+            raise Exception("Incorrect hand length")
+        # TODO: Finish _calc_rank method
+        return (1,1,1,1,1)
+    
+    def compare(self, other):
+        # TODO Compare hands
+        pass
 
 class Deck:
         def __iter__(self):
@@ -36,118 +90,113 @@ class Deck:
                 self.count += 1
                 return card
 
-
 class Player:
-    def __init__(self, deck):
+    def __init__(self, deck, stack):
         # contains the two cards that this player has in their hand
         self.cards = [next(deck), next(deck)]
         # How much money they have
-        self.stack = 100.0
+        self.stack = stack
         # Is this person currently out of the hand
         self.folded = False
-        # The type of hand that this person has
-        self.score = Hand.HIGHCARD
-        # The actual cards that make up this hand
-        self.hand = []
+        # The Hand object of their current best possible Hand
+        self.best_hand = None
+    
+    def set_best_hand(self, board):
+        # TODO set_best_hand
+        # Should go through all possible combinations of 5 card hands and compare them with the Hand.compare
+        #flop
+        if len(board) == 3:
+            pass
+        #turn
+        if len(board) == 4:
+            pass
+        #river
+        if len(board) == 5:
+            pass
 
 class Game:
     def __init__(self, num_players):
+        # create the iterable deck to deal cards from
         self.deck = iter(Deck())
-        self.players = [Player(self.deck) for i in range(num_players)]
+        # create a list of Player objects
+        if num_players > 9 or num_players < 1:
+            raise Exception("Invalid Player Ammount")
+        self.players = [Player(self.deck, 100) for i in range(num_players)]
+        # The board of common cards
         self.board = []
     
     def _deal_cards(self, num):
-        # deal n cards from the deck onto the board
+        # Deal num cards from the deck onto the board
         for i in range(num):
             self.board.append(next(self.deck))
-    
-    def _check_player_best_hand(self, player):
-        # for each card in player's hand
-        for card in player.cards:
-            # exclude one card from the board
-            for c2 in self.board:
-                # include card and exclude c2
-                lst = [i for i in self.board if i != c2]
-                lst.append(card)
-                lst.sort(key=lambda _ : RANK.index(_[0]))
-                # Straight flush
-                if isFlush(lst):
-                    if isStraight(lst):
-                        if player.score < Hand.STRAIGHTFLUSH:
-                            player.score = Hand.STRAIGHTFLUSH
-                            player.hand = lst.copy()
-                            continue
-                        else:  # player.score == Hand.STRAIGHTFLUSH:
-                            #TODO Check which one is better
-                            pass
-                    # Flush
-                    else:
-                        if player.score < Hand.FLUSH:
-                            player.score = Hand.FLUSH
-                            player.hand = lst.copy()
-                            continue
-                        elif player.score == Hand.FLUSH:
-                            #TODO check which is better
-                            pass
-                
-                # Straight
-                if isStraight(lst):
-                    if player.score < Hand.STRAIGHT:
-                        player.score = Hand.STRAIGHTFLUSH
-                        player.hand = lst.copy()
-                        continue
-                    elif player.score == Hand.STRAIGHT:
-                        # TODO
-                        pass
-                # 4oK
-                if is4ok(lst):
 
-                # Boat 
-                
-                # 3oK
-                # 2P
-                # Pair
-                # HK
-                print(lst)
-                exit()
 
-    def determine_winner(self):
-        print("Calculating Winner...")
+    def _determine_winner(self):
+        # TODO Fix for ties
+        # Compares all of the players' best_hands and returns the winner
+        best = None
         for player in self.players:
-            if player.folded:
-                continue
-            else:
-                #TODO: determine winner
-                self._check_player_best_hand(player)
+            if not player.folded:
+                if best == None:
+                    best = player
+                else:
+                    # if the player has a better best_hand than best, then set best to player
+                    if best.best_hand.compare(player.best_hand) < 0:
+                        best = player
+        return player
+
+    def display_boad(self):
+        # TODO Graphics
+        print(self.board)
+
+    def bet_round(self, preflop=False):
+        # TODO bet_round
+        input() # temporary for pausing
+        pass
+    
+    def calc_hands(self):
+        # This function should be called after any card is delt to calculate each player's new best hand
+        for player in self.players:
+            player.set_best_hand(self.board)
 
     def flop(self):
+        # Deal cards
         self._deal_cards(3)
-        print("Flop: ", self.board)
-    
+        # Display board
+        print("Flop")
+        self.display_boad()
+        self.calc_hands()
+        self.bet_round()
+            
     def turn(self):
         self._deal_cards(1)
-        print("Turn: ", self.board)
+        print("Turn")
+        self.display_boad()
+        self.calc_hands()
+        self.bet_round()
     
     def river(self):
         self._deal_cards(1)
-        print("River: ", self.board)
+        print("River")
+        self.display_boad()
+        self.calc_hands()
+        self.bet_round()
 
-    def main(self):
+    def run(self):
         print("Starting Game...")
 
+        # Print cards (temporarily)
         for player in self.players:
             print(player.cards)
 
-        input()
+        self.bet_round(preflop=True)
+
         self.flop()
-        input()
         self.turn()
-        input()
         self.river()
-        input()
 
         self.determine_winner()
 
 if __name__ == "__main__":
     holdem = Game(1)
-    holdem.main()
+    holdem.run()
